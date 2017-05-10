5e2f0bb3f60a91891b435514592587241acb8f40
George Stocker <george.stocker@vaeit.com>
2017-05-10 18:15:56 -0400

    Reproduces the issue with the POST endpoint

--- src/Program.cs
+++ src/Program.cs
@@ -3,51 +3,64 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace HttpClientAsync
 {
     class Program
     {
+        private static string getEndpoint = "http://webservice/webservice/hello";
+        private static string postEndpoint = "http://webservice/webservice/";
         static void Main(string[] args)
         {
-            var times = new Dictionary<long, long>();
+            
 
-            Console.WriteLine("Starting benchmark Run, iterations: 100000");
+            Console.WriteLine("Starting benchmark Run, iterations: 100000, 5 times");
             using (HttpClientExecutor executor = new HttpClientExecutor())
             {
-                var i = 0;
-                var max = 0L;
-                var run = 0L;
-                string endpoint = "http://api-test:8080/webservice/hello";
-                while (i < 100000)
+                for (var iter = 0; iter < 5; iter++)
                 {
-
-                    var localRun = executor.Execute(endpoint);
-                    if (!times.ContainsKey(localRun))
+                    var i = 0;
+                    var max = 0L;
+                    var run = 0L;
+                    
+                    var times = new Dictionary<long, long>();
+                    while (i < 10)
                     {
-                        times.Add(localRun, 1);
+                        long localRun;
+                        localRun = args[0] == "GET" ? executor.ExecuteGet(getEndpoint) : executor.ExecutePost(postEndpoint);
+                        if (!times.ContainsKey(localRun))
+                        {
+                            times.Add(localRun, 1);
+                        }
+                        else
+                        {
+                            times[localRun]++;
+                        }
+                        if (localRun > max)
+                        {
+                            max = localRun;
+                        }
+                        run += localRun;
+                        i++;
                     }
-                    else
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("RUN {0}", iter);
+                    Console.WriteLine("------");
+                    Console.WriteLine("Number of Runs: {0} ms", i);
+                    Console.WriteLine("Average run is {0} ms", run / i);
+                    Console.WriteLine("High Request was: {0} ms", max);
+                    Console.WriteLine("-------      -------");
+                    foreach (var item in times)
                     {
-                        times[localRun]++;
+                        Console.WriteLine("Run Time {0} (ms): Occurences: {1}", item.Key, times[item.Key]);
                     }
-                    if (localRun > max)
-                    {
-                        max = localRun;
-                    }
-                    run += localRun;
-                    i++;
+                    Console.WriteLine("-----------------------");
                 }
-                Console.WriteLine("Number of Runs: {0} ms", i);
-                Console.WriteLine("Average run is {0} ms", i / run);
-                Console.WriteLine("High Request was: {0} ms", max);
-
             }
-            Console.WriteLine("End Run");
-            foreach (var item in times.OrderBy(k => k.Key))
-            {
-                Console.WriteLine("Run Time {0} (ms): Occurences: {1}", item.Key, times[item.Key]);
-            }
+            Console.WriteLine("End Runs");
+           
         }
     }
 
@@ -63,36 +76,42 @@ namespace HttpClientAsync
             }
         }
 
-
-        public long Execute(string endpoint)
+        private HttpClient GetHttpClient(Uri endpoint)
         {
-
-            if (clients.TryGetValue(endpoint, out HttpClient client))
+            if (clients.TryGetValue(endpoint.Host, out HttpClient client))
             {
-                    var sw = new Stopwatch();
-                   
-                    sw.Start();
-                    var returnString = client.GetStringAsync(new Uri(endpoint, UriKind.Absolute)).Result;
-                    sw.Stop();
-                    return sw.ElapsedMilliseconds;
-                    
+                return client;
             }
             else
             {
-                clients.Add(endpoint, new HttpClient());
-                    var sw = new Stopwatch();
-                  
-                    sw.Start();
-                    var returnString = clients[endpoint].GetStringAsync(new Uri(endpoint, UriKind.Absolute)).Result;
-                    sw.Stop();
-                   
-                    return sw.ElapsedMilliseconds;
+                clients.Add(endpoint.Host, new HttpClient());
+                return clients[endpoint.Host];
             }
         }
+        public long ExecuteGet(string endpoint)
+        {
+            var sw = new Stopwatch();    
+            sw.Start();
+            var returnResponse = GetHttpClient(new Uri(endpoint)).GetAsync(endpoint).Result;
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+
         public void Dispose() {
             foreach (var client in clients.Keys) {
               clients[client].Dispose();
             }
+        }
+
+        public long ExecutePost(string postEndpoint)
+        {
+            var url = new Uri(postEndpoint);
+            var content = new StringContent(JsonConvert.SerializeObject(new {value = "value"}), Encoding.UTF8, "application/json");
+            var sw = new Stopwatch();
+            sw.Start();
+            var returnString = GetHttpClient(url).PostAsync(url, content).Result;
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
         }
     }
     
